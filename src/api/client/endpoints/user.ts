b03b6d646c9a4f5fc8dd84f747e992f0d519e06f
Dexter De Leon <dexter@aimusic.co.uk>
2020-04-07 15:22:32 +0100

    feat(User): Add user endpoint
    Add user endpoint
    
    feat(User): Update user endpoints
    
    Update user endpoints
    
    feat(User): Update user get path
    
    Update user get path
    
    refactor(Client): Fix UserEndpoint import
    
    Fix UserEndpoint import
    
    feat(User): Add user list method
    
    Add user list method

---
+++ src/api/client/endpoints/user.ts
@@ -0,0 +1,63 @@
+import { ApiResponse, ENDPOINT, IHttpClient, IUserCreate, IUserDashboard, IUserFavourites, IUserPayload, IUserActivate } from '../../../types'
+
+export class UserEndpoint {
+  public path = `${ENDPOINT.USER}`
+  constructor(protected readonly client: IHttpClient) {}
+
+  /**
+   * This method will allow you to create a user
+   * @param payload
+   */
+  public create(payload: IUserCreate): ApiResponse<IUserPayload> {
+    return this.client.post<IUserCreate, IUserPayload>(this.path, payload)
+  }
+
+  /**
+   * This method will retrieve the authenticated user if called without a userId
+   * Otherwise it will retrieve the user that matches the userId parameter
+   * @param userId
+   */
+  public get(userId = ''): ApiResponse<IUserPayload> {
+    return this.client.get<IUserPayload>(`${this.path}${userId && `/${userId}`}`)
+  }
+
+  /**
+   * This method will retrieve a collection of users
+   */
+  public list(): ApiResponse<IUserPayload> {
+    return this.client.get<IUserPayload>(`${this.path}`)
+  }
+
+  /**
+   * This method will update the user model with the given payload
+   * @param payload
+   */
+  public update(payload: IUserPayload): ApiResponse<IUserPayload> {
+    return this.client.put<IUserPayload>(`${this.path}/${payload.id}`, payload)
+  }
+
+  /**
+   * This method will activate the user matching the userId
+   * @param payload
+   */
+  public activate(payload: IUserActivate, userId: string): ApiResponse<IUserPayload> {
+    return this.client.patch<IUserActivate, IUserPayload>(`${this.path}/${userId}/${ENDPOINT.ACTIVATE}`, payload)
+  }
+
+  /**
+   * This method will retrieve the data for a user's dashboard
+   * @param userId
+   */
+  public dashboard(userId: string): ApiResponse<IUserDashboard> {
+    return this.client.get<IUserDashboard, string>(`${this.path}/${userId}/${ENDPOINT.DASHBOARD}`)
+  }
+
+  /**
+   * This method will add a backingTrack ID to a user's favourites collection
+   * @param payload
+   * @param userId
+   */
+  public favourites(payload: IUserFavourites, userId: string): ApiResponse<IUserPayload> {
+    return this.client.post<IUserFavourites, IUserPayload>(`${this.path}/${userId}/${ENDPOINT.FAVOURITES}`, payload)
+  }
+}
